4be0fa80ba9f327230ab29df9ef1eb76f8c4c714
Lokesh Boddula <54484862+lokeshboddula@users.noreply.github.com>
2023-04-29 17:02:39 +0530

    assignment complete

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -10,16 +10,52 @@ public class Car extends Vehicle {
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name);
         //Hint: Car extends Vehicle
+        currentGear = 1;
+        this.type = type;
+        this.seats = seats;
+        this.doors = doors;
+        this.wheels = wheels;
+        this.isManual = isManual;
+        this.gears = gears;
     }
 
     public void changeGear(int newGear){
-
+        currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
     }
 }
